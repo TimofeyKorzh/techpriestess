190ba4763632a88759518255c5ba085fb2db7b71
TimofeyKorzh <tkorghebin@gmail.com>
2021-09-22 07:50:47 +0000

    security2

--- config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'token': 'NTgwNzUyMjkzODE3ODc2NTEw.XOVRuA.24hhfdcFX70mVTZaUopgDGEF4oA',
-    'bot': 'Инфо-экзекутор Аля',
-    'id': 5688,
-    'prefix': ['/', 'Аля ', 'Аля, ', 'X-439-Альфа, ', 'X-439-Alpha, ']
-}
